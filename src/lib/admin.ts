852c7171f0bb22ca8b7edd0dc1435486b7319b2e
Ernő Sántha <erno.santha@gmail.com>
2025-10-23 21:01:46 +0000

    ezt a szót majd hol kell megadni?

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -3,7 +3,7 @@
 // For now, we will use a hardcoded admin UID.
 // In a production app, this should be managed via a database role or a custom claim.
 // TODO: Replace this with your actual Firebase User ID for the admin user.
-export const ADMIN_UID = "Patika2025";
+export const ADMIN_UID = "bjoekd4vPaNKIrw2E39UlPqDbxj1";
 
 export const isAdmin = (uid: string | undefined): boolean => {
     return uid === ADMIN_UID;
