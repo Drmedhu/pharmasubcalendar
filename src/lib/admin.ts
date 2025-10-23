a6f09cc25e2e6e1db0fdc7027de6a0bf20d8d14d
Ernő Sántha <erno.santha@gmail.com>
2025-10-23 21:00:36 +0000

    az admin egyedi azonosítóját IUD javítsd ki mindenhol az alábbi szóra: P

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -3,7 +3,7 @@
 // For now, we will use a hardcoded admin UID.
 // In a production app, this should be managed via a database role or a custom claim.
 // TODO: Replace this with your actual Firebase User ID for the admin user.
-export const ADMIN_UID = "REPLACE_WITH_YOUR_ADMIN_FIREBASE_UID";
+export const ADMIN_UID = "Patika2025";
 
 export const isAdmin = (uid: string | undefined): boolean => {
     return uid === ADMIN_UID;
