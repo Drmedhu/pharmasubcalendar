13dbee499368617c08996d6e7e990f050dcaeaeb
Ernő Sántha <erno.santha@gmail.com>
2025-10-23 15:37:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3758582175-2db24",
+  "appId": "1:243422162135:web:f7f3dc4470f4479d601492",
+  "apiKey": "AIzaSyDgHs853QT7YADyHV0Tj6LYtCx_RZ00Qto",
+  "authDomain": "studio-3758582175-2db24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "243422162135"
+};
